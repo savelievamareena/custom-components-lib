be05a503ab2c7f2894e4ea42f432beb2a10879c6
savelievamareena <savelieva.mareena.1.21@gmail.com>
2024-04-03 18:08:57 +0300

    feat: add Button component

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,32 +1,32 @@
 import { type StoryObj, type Meta } from "@storybook/react";
 import Button from "./Button";
 import { type ButtonProps } from "./Button.types";
-import "./Button.scss";
 
 export default {
     title: "Components/Button",
     component: Button,
 } as Meta;
 
-export const Primary: StoryObj<ButtonProps> = {
-    args: {
-        variant: "text",
-        size: "large",
-        children: "My Button",
-    },
-};
-
 export const Large: StoryObj<ButtonProps> = {
     args: {
         variant: "text",
         size: "large",
-        children: "My Button",
+        children: "LARGE",
+    },
+};
+
+export const Medium: StoryObj<ButtonProps> = {
+    args: {
+        size: "medium",
+        children: "MEDIUM",
+        variant: "contained",
     },
 };
 
 export const Small: StoryObj<ButtonProps> = {
     args: {
         size: "small",
-        children: "My Button",
+        children: "SMALL",
+        variant: "outlined",
     },
 };
